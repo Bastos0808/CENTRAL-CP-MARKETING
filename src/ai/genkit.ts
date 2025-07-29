b0e7bf2092d419eb4d97cd2e2c07549ca6f52d92
Gabriell Bastos <gabriellbastos0825@gmail.com>
2025-07-29 12:35:41 +0000

    2025-07-29T12:32:34.335Z [error] ⨯ Error [GenkitError]: FAILED_PRECONDIT

--- src/ai/genkit.ts
+++ src/ai/genkit.ts
@@ -4,7 +4,7 @@ import {googleAI} from '@genkit-ai/googleai';
 export const ai = genkit({
   plugins: [
     googleAI({
-      apiKey: process.env.GEMINI_API_KEY,
+      apiKey: process.env.GEMINI_API_KEY ?? "AIzaSyBL-nMAyZEV-okxM5ki3W4mFt8HJtSNOo4",
     }),
   ],
   logLevel: 'debug',
