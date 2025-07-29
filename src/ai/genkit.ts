2db017a5ba754dc75111d262d6830fd2d8d6ab41
Gabriell Bastos <gabriellbastos0825@gmail.com>
2025-07-29 12:39:47 +0000

    AIzaSyC7f7CDPD-VQF8kHupEOEUAXqXHgXe_fwI use essa chave de api

--- src/ai/genkit.ts
+++ src/ai/genkit.ts
@@ -4,7 +4,7 @@ import {googleAI} from '@genkit-ai/googleai';
 export const ai = genkit({
   plugins: [
     googleAI({
-      apiKey: process.env.GEMINI_API_KEY ?? "AIzaSyBL-nMAyZEV-okxM5ki3W4mFt8HJtSNOo4",
+      apiKey: process.env.GEMINI_API_KEY ?? "AIzaSyC7f7CDPD-VQF8kHupEOEUAXqXHgXe_fwI",
     }),
   ],
   logLevel: 'debug',
