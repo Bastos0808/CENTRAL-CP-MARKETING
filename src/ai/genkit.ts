b4198cdf04abbe46037bb7fff7226450cfff414a
Gabriell Bastos <gabriellbastos0825@gmail.com>
2025-07-29 12:30:38 +0000

    AIzaSyBL-nMAyZEV-okxM5ki3W4mFt8HJtSNOo4 use essa chave api

--- src/ai/genkit.ts
+++ src/ai/genkit.ts
@@ -1,15 +1,12 @@
 import {genkit} from 'genkit';
 import {googleAI} from '@genkit-ai/googleai';
-import {defineConfig} from 'genkit';
 
-export const ai = genkit(
-  defineConfig({
-    plugins: [
-      googleAI({
-        apiKey: process.env.GEMINI_API_KEY,
-      }),
-    ],
-    logLevel: 'debug',
-    enableTracingAndMetrics: true,
-  })
-);
+export const ai = genkit({
+  plugins: [
+    googleAI({
+      apiKey: process.env.GEMINI_API_KEY,
+    }),
+  ],
+  logLevel: 'debug',
+  enableTracingAndMetrics: true,
+});
