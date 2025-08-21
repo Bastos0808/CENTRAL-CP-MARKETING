328f14a5d0419ca0b00de910eb8afd1f6f65ac2f
Gabriell Bastos <gabriellbastos0825@gmail.com>
2025-08-21 23:56:02 +0000

    o erro está relacionado a ia geral do app

--- src/ai/genkit.ts
+++ src/ai/genkit.ts
@@ -3,9 +3,6 @@ import {googleAI} from '@genkit-ai/googleai';
 
 export const ai = genkit({
   plugins: [
-    googleAI({
-      apiKey: process.env.GEMINI_API_KEY,
-    }),   
+    googleAI(),   
   ],
-  enableTracingAndMetrics: true,
 });
