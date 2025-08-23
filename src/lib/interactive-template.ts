48d9f4d661bf33f5c5d8a6cf792299288d325628
Gabriell Bastos <gabriellbastos0825@gmail.com>
2025-08-23 14:18:41 +0000

    Error: ./src/lib/interactive-template.ts:285:10 Parsing ecmascript sourc

--- src/lib/interactive-template.ts
+++ src/lib/interactive-template.ts
@@ -282,7 +282,7 @@ export function createInteractiveProposal(data: CreateProposalData): string {
       }
   ];
 
-  return \`<!DOCTYPE html>
+  return `<!DOCTYPE html>
 <html lang="pt-br">
 <head>
     <meta charset="UTF-8">
@@ -933,5 +933,5 @@ export function createInteractiveProposal(data: CreateProposalData): string {
         };
     </script>
 </body>
-</html>\`;
+</html>`;
 }
